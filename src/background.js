3a234829448bd91127c10cfc5b7f15d2afca6a93
MechaDragonX <sonicraghav@gmail.com>
2019-10-16 13:44:23 -0700

    Add backgrounod functions

--- src/background.js
+++ src/background.js
@@ -1,5 +1,38 @@
-var playingTab
 var curentTab;
 var playing = false;
 var audio = new Audio();
+audio.loop = true;
 
+browser.runtime.onMessage.addListener(function handleMessage(request, sender, sendResponse) {
+    if(request.onMessage == "wii") {
+        browser.tabs.query( {
+            active: true, currentWindow: true
+        }, function(tabs) {
+            playMusic(tabs)
+        });
+    }
+});
+
+browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    currentTab = tab.id;
+    // TODO: Add audio files
+    if(tab.url == "*://*amazon.co.jp/*") /*audio.src = "data/オーディオ.ogg"*/ console.log("アマゾンへようこそ！");
+    else /*audio.src = "data/audio.ogg"*/ console.log("Welcome to Amazon!");
+});
+
+function playMusic(tabs) {
+    if(!playing) {
+    //   audio.load();
+    playing = true;
+    //   audio.play();
+    console.log("Music playing!");
+    }
+}
+
+browser.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    if(tabId == currentTab) {
+        playing = false;
+        // audio.remove();
+        console.log("Music stopped!");
+    }
+});
